aa377322579af3ad8911fcb8a8709ba8c8840e66
yashdeepdeshmukh18 <ybdeshmukh123@gmail.com>
2026-03-01 23:49:11 +0530

    Updated the catalog page

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -109,6 +109,7 @@ function Catalog() {
           />
         </div>
       </div>
+
       {/* Section 2 */}
       <div className=" mx-auto box-content w-full max-w-maxContentTab px-4 py-12 lg:max-w-maxContent">
         <div className="section_heading">
