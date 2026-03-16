2e27395878d2d8e8bd06dde47c1aeba03f63321e
yashdeepdeshmukh18 <ybdeshmukh123@gmail.com>
2026-03-16 23:21:41 +0530

    Corrected the courseID to courseId

--- server/controllers/Payments.js
+++ server/controllers/Payments.js
@@ -164,7 +164,7 @@ const enrollStudents = async (courses, userId, res) => {
       console.log("Updated course: ", enrolledCourse)
 
       const courseProgress = await CourseProgress.create({
-        courseID: courseId,
+        courseId: courseId,
         userId: userId,
         completedVideos: [],
       })
