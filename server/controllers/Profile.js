7d162cd3be3415ff20393e08846d1e9a7b02613f
yashdeepdeshmukh18 <ybdeshmukh123@gmail.com>
2026-03-16 23:24:27 +0530

    Added the courseProgressCount logic in getEnrolledCourses

--- server/controllers/Profile.js
+++ server/controllers/Profile.js
@@ -1,6 +1,8 @@
 const Profile = require("../models/Profile");
 const User = require("../models/User");
 const {uploadImageToCloudinary} = require("../utils/imageUploader");
+const { convertSecondsToDuration } = require("../utils/secToDuration");
+const CourseProgress = require("../models/CourseProgress");
 
 exports.updateProfile = async (req, res) => {
     try{
@@ -139,30 +141,101 @@ exports.updateDisplayPicture = async (req, res) => {
 };
 
 
-exports.getEnrolledCourses = async (req, res) => {
-    try {
-      const userId = req.user.id
-      const userDetails = await User.findOne({
-        _id: userId,
-      })
-        .populate("courses")
-        .exec()
+// exports.getEnrolledCourses = async (req, res) => {
+//     try {
+//       const userId = req.user.id
+//       const userDetails = await User.findOne({
+//         _id: userId,
+//       })
+//         .populate("courses")
+//         .exec()
 
-      if (!userDetails) {
-        return res.status(400).json({
-          success: false,
-          message: `Could not find user with id: ${userDetails}`,
-        })
-      }
-      return res.status(200).json({
-        success: true,
-        data: userDetails.courses,
+//       if (!userDetails) {
+//         return res.status(400).json({
+//           success: false,
+//           message: `Could not find user with id: ${userDetails}`,
+//         })
+//       }
+//       return res.status(200).json({
+//         success: true,
+//         data: userDetails.courses,
+//       })
+//     } catch (error) {
+//       return res.status(500).json({
+//         success: false,
+//         message: error.message,
+//       })
+//     }
+// };
+
+exports.getEnrolledCourses = async (req, res) => {
+  try {
+    const userId = req.user.id
+    let userDetails = await User.findOne({
+      _id: userId,
+    })
+      .populate({
+        path: "courses",
+        populate: {
+          path: "courseContent",
+          populate: {
+            path: "subSection",
+          },
+        },
       })
-    } catch (error) {
-      return res.status(500).json({
+      .exec()
+
+    userDetails = userDetails.toObject()
+
+    var SubsectionLength = 0
+
+    for (var i = 0; i < userDetails.courses.length; i++) {
+      let totalDurationInSeconds = 0
+      SubsectionLength = 0
+
+      for (var j = 0; j < userDetails.courses[i].courseContent.length; j++) {
+        totalDurationInSeconds += userDetails.courses[i].courseContent[
+          j
+        ].subSection.reduce((acc, curr) => acc + parseInt(curr.timeDuration), 0)
+        userDetails.courses[i].totalDuration = convertSecondsToDuration(
+          totalDurationInSeconds
+        )
+        SubsectionLength +=
+          userDetails.courses[i].courseContent[j].subSection.length
+      }
+
+      let courseProgressCount = await CourseProgress.findOne({
+        courseId: userDetails.courses[i]._id,
+        userId: userId,
+      })
+
+      courseProgressCount = courseProgressCount?.completedVideos.length;
+      if (SubsectionLength === 0) {
+        userDetails.courses[i].progressPercentage = 100
+      } else {
+        // To make it up to 2 decimal point
+        const multiplier = Math.pow(10, 2)
+        userDetails.courses[i].progressPercentage =
+          Math.round(
+            (courseProgressCount / SubsectionLength) * 100 * multiplier
+          ) / multiplier
+      }
+    }
+
+    if (!userDetails) {
+      return res.status(400).json({
         success: false,
-        message: error.message,
+        message: `Could not find user with id: ${userDetails}`,
       })
     }
-};
-
+    return res.status(200).json({
+      success: true,
+      data: userDetails.courses,
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    })
+  }
+}
