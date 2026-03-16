1adde73656930b6112d88e21bd3c9274830a1c72
yashdeepdeshmukh18 <ybdeshmukh123@gmail.com>
2026-03-16 23:17:46 +0530

    Changed the printing stmt

--- server/controllers/Category.js
+++ server/controllers/Category.js
@@ -143,7 +143,7 @@ exports.categoryPageDetails = async (req, res) => {
       })
       .exec()
 
-    console.log("SELECTED COURSE", selectedCategory)
+    console.log("SELECTED Category", selectedCategory)
 
     // Handle the case when the category is not found
     if (!selectedCategory) {
