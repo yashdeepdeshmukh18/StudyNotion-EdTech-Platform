49927e704477938c9b37c75d0b61ae728a527e77
yashdeepdeshmukh18 <ybdeshmukh123@gmail.com>
2026-03-23 01:27:20 +0530

    Changed the project name

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -11,7 +11,7 @@ const mailSender = async (email, title, body) => {
         })
 
         let info = await transporter.sendMail({
-            form: 'StudyNotion || CodeHelp - by babbar',
+            form: 'Learnify || CodeHelp - by babbar',
             to:`${email}`,
             subject: `${title}`,
             html: `${body}`
